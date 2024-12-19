a23e4073739ba0be51ebea3b8074d8142f5a772f
SumeraAttar <161694604+SumeraAttar@users.noreply.github.com>
2024-12-19 15:44:32 +0530

    Update index.js

--- index.js
+++ index.js
@@ -592,7 +592,7 @@ app.get("*", (req, res) => {
 
 
 
-const port = process.env.PORT || 5001;
+const port = process.env.PORT || 5002;
 app.listen(port, () => {
     console.log(Server running on port ${port});
 });
