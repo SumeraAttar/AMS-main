fa051e29f61375926597bbf0fbdce3b8f4840f0e
SumeraAttar <161694604+SumeraAttar@users.noreply.github.com>
2024-12-19 16:57:03 +0530

    Update config.js

--- db/config.js
+++ db/config.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 mongoose.connect(
-  "mongodb+srv://aniketgawai88:aniket123@ams.lirqsoi.mongodb.net/ams?retryWrites=true&w=majority&appName=AMS",
+  "mongodb+srv://Sumera:12345@cluster0.viuy4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
   {
     serverSelectionTimeoutMS: 50000, // 30 seconds
     socketTimeoutMS: 45000, // 45 seconds
